63ae0d11e5350be157e5466586fceacff9a4e0a3
Brian Witt <briantwitt@gmail.com>
2015-03-21 17:13:57 -0400

    added boolean to indicate whether current user is up for each of the events shown

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -8,18 +8,21 @@ angular.module('starter.controllers', [])
         name:"Startup Weekend", 
         date: "3/21/15", 
         imgUrl: "https://www.filepicker.io/api/file/AQR9NAIiTw6grdY5OpWn?signature=80b5fd73e82069fda0bf5c96decc872f123e86ee60bea9358441babbbc44206f&policy=eyJleHBpcnkiOjE0MjY5Nzg4MTksImNhbGwiOlsicGljayIsInJlYWQiLCJ3cml0ZSIsInN0b3JlIl19",
-        peopleCount: 3
+        upForItCount: 3,
+        userIsUpForIt: false
       },
       {
         name:"Katy Perry",
         date: "3/26/15",
         imgUrl: "http://s1.ticketm.net/tm/en-us/dbimages/163544a.jpg",
-        peopleCount: 6
+        upForItCount: 6,
+        userIsUpForIt: false
       },
       {
         name:"Jog Around Cobbs Hill", 
         date: "tonight",
-        peopleCount: 2
+        upForItCount: 2,
+        userIsUpForIt: true
       }
     ];
 
