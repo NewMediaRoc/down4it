52a7457949dda1f3772f9b8d98328b09f9b89a9e
Brian Witt <briantwitt@gmail.com>
2015-03-21 16:33:02 -0400

    added counts to each event on the events screen

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,9 +4,23 @@ angular.module('starter.controllers', [])
   .controller('MeCtrl', function($scope) {})
   .controller('EventsCtrl', function($scope) {
     $scope.items = [
-      {name:"Startup Weekend", date: "3/21/15", imgUrl: "https://www.filepicker.io/api/file/AQR9NAIiTw6grdY5OpWn?signature=80b5fd73e82069fda0bf5c96decc872f123e86ee60bea9358441babbbc44206f&policy=eyJleHBpcnkiOjE0MjY5Nzg4MTksImNhbGwiOlsicGljayIsInJlYWQiLCJ3cml0ZSIsInN0b3JlIl19" },
-      {name:"Katy Perry", date: "3/26/15", imgUrl: "http://s1.ticketm.net/tm/en-us/dbimages/163544a.jpg" },
-      {name:"Jog Around Cobbs Hill", date: "tonight" }
+      {
+        name:"Startup Weekend", 
+        date: "3/21/15", 
+        imgUrl: "https://www.filepicker.io/api/file/AQR9NAIiTw6grdY5OpWn?signature=80b5fd73e82069fda0bf5c96decc872f123e86ee60bea9358441babbbc44206f&policy=eyJleHBpcnkiOjE0MjY5Nzg4MTksImNhbGwiOlsicGljayIsInJlYWQiLCJ3cml0ZSIsInN0b3JlIl19",
+        peopleCount: 3
+      },
+      {
+        name:"Katy Perry",
+        date: "3/26/15",
+        imgUrl: "http://s1.ticketm.net/tm/en-us/dbimages/163544a.jpg",
+        peopleCount: 6
+      },
+      {
+        name:"Jog Around Cobbs Hill", 
+        date: "tonight",
+        peopleCount: 2
+      }
     ];
 
 
