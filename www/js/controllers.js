27a57ac0baa129431eafee424e4cd75b45e97193
Brian Witt <briantwitt@gmail.com>
2015-03-21 19:50:49 -0400

    link from events page to specific event page

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -6,12 +6,18 @@ angular.module('starter.controllers', [])
     $scope.events = events.all();
   })
   .controller('EventsNewCtrl', function($scope) {})
-  .controller('EventCtrl', function($scope, $stateParams, events) {
+  .controller('EventCtrl', function($scope, $stateParams, events, users) {
     console.log($stateParams.eventId);
 //Use $window.innerHeight to get the height of the viewport
 
     $scope.event = events.get($stateParams.eventId);
-    $scope.users = $scope.event.users;
+    $scope.users = [];
+    for (var i = 0; i < $scope.event.users.length; i++) {
+      $scope.users.push({
+        preferences: $scope.event.users[i].userPreferences,
+        profile: users.get($scope.event.users[i].id)
+      });
+    }   
   })
   .controller('ProfileCtrl', function($scope, $stateParams, users) {
     console.log($stateParams.userId);
